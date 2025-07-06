8d459c30d0a100be9cbc23e6db6edfef5ba546ea
MuraliKrishna-devops <muralikrishnadevops24@gmail.com>
2025-07-06 15:54:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold text-center text-blue-600">Welcome to Chandu Mart</h1>
+      <p className="text-center mt-4">Your Mart, Your Share – Only at Chandu Mart!</p>
+    </div>
+  );
+}
+
+export default App;
